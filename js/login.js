bebaef42e9c89f2a63ebaf1366329c1594bf8fd5
nagendra5486 <khambhamnagendra@gmail.com>
2024-07-23 22:35:24 +0530

    changes

--- js/login.js
+++ js/login.js
@@ -1,10 +1,35 @@
 function login_user(){
+    debugger;
 
+    const username=document.getElementById('username').value;
+    console.log(username);
+    const password =document.getElementById('password').value;
+    console.log(password)
 
-    alert('login user Sussessfully')
+    const userDetails={
+        "username":username,
+        "password":password,
+    }
+    console.log(userDetails);
+
+    localStorage.setItem('userlogin',JSON.stringify(userDetails))
+
+    console.log(username === "apj" && password === "123")
+if(username === "apj" && password === "123"){
+
+console.log("login page")
+}
+else{
+    console.log("not sucessfull")
+}
 }
 
 function cancel(){
 
     alert("cancel")
+}
+
+
+function singup(){
+    alert("you singingup")
 }
